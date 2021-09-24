12710ea52ec112f3c605293bb365fff43b4630cd
Kindi-Equinox <91266152+Kindi-Equinox@users.noreply.github.com>
2021-09-25 02:32:36 +0800

    Add files via upload

--- Protective Guards for OpenMW - Lua Edition/protective_guards_for_omw/global.lua
+++ Protective Guards for OpenMW - Lua Edition/protective_guards_for_omw/global.lua
@@ -1,14 +1,18 @@
 local core = require("openmw.core")
 local query = require("openmw.query")
+local aux = require("openmw_aux.util")
+local searchedCells = {}
 local pursuit_for_omw = false
 
-
-
 local function searchGuards(data)
-	if not pursuit_for_omw then
-		return
-	end
+    if not pursuit_for_omw then
+        return
+    end
     local door, agg = unpack(data)
+    if searchedCells[tostring(door)] then
+        return
+    end
+    searchedCells[tostring(door)] = true
     local adjacentCellActors = door.destCell:selectObjects(query.actors)
     for _, actor in adjacentCellActors:ipairs() do
         if
@@ -24,17 +28,38 @@ local function searchGuards(data)
     end
 end
 
+aux.runEveryNSeconds(
+    10,
+    function()
+        searchedCells = {}
+    end
+)
+
 return {
     engineHandlers = {
         onActorActive = function(actor)
+            if not actor then
+                return
+            end
             actor:addScript("protective_guards_for_omw/aggressor.lua")
             if actor.type == "NPC" then
                 actor:addScript("protective_guards_for_omw/protect.lua")
             end
+        end,
+        onLoad = function()
+            aux.runEveryNSeconds(
+                10,
+                function()
+                    searchedCells = {}
+                end
+            )
         end
     },
     eventHandlers = {
         searchGuards = searchGuards,
-		pursuit_for_omw_installed = function() pursuit_for_omw = true print("Pursuit and Protective Guards interaction established") end
+        pursuit_for_omw_installed = function()
+            pursuit_for_omw = true
+            print("Pursuit and Protective Guards interaction established")
+        end
     }
 }
