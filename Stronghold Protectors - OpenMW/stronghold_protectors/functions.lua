719e8ac4efdcae92bafe9363370305c939d56264
Kindi-Equinox <91266152+Kindi-Equinox@users.noreply.github.com>
2021-09-26 17:52:10 +0800

    update

---
+++ Stronghold Protectors - OpenMW/stronghold_protectors/functions.lua
@@ -0,0 +1,40 @@
+local stronghold = require("stronghold_protectors.strongholds")
+local this = {}
+
+
+
+
+this.checkCellIsStronghold = function (cell)
+    for _, v in pairs(stronghold) do
+        if cell:match(v) then
+            return true
+        end
+    end
+    return false
+end
+
+this.isOwnerHere = function (nearby)
+    for _, actor in nearby.actors:ipairs() do
+        if
+            actor:isValid() and actor.type == "NPC" and
+            (actor.recordId == "banden indarys" or actor.recordId == "raynasa rethan" or
+            actor.recordId == "reynel uvirith")
+        then
+            return true
+        end
+    end
+    return false
+end
+
+
+
+
+
+
+
+
+
+
+
+
+return this--phucking phunctions
