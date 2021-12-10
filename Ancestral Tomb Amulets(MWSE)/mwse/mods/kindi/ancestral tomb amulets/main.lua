e2845bc634fbd3dbc8d5629cb8ba5c4c4e2a9f5d
Kindi-Equinox <91266152+Kindi-Equinox@users.noreply.github.com>
2021-12-10 14:33:45 +0800

    comment

--- Ancestral Tomb Amulets(MWSE)/mwse/mods/kindi/ancestral tomb amulets/main.lua
+++ Ancestral Tomb Amulets(MWSE)/mwse/mods/kindi/ancestral tomb amulets/main.lua
@@ -195,7 +195,7 @@ local function closeAtaTableRC()
     end
 end
 
-local function getall()
+--[[local function getall()
     for a in tes3.getPlayerCell():iterateReferences(tes3.objectType.container) do
         for k, v in pairs(a.object.inventory) do
             if v.object.id:match("ata_kindi_amulet") then
@@ -207,7 +207,7 @@ local function getall()
     tes3ui.updateInventoryTiles()
     amuletCreationCellRecycle(tes3.getPlayerCell())
 end
-event.register("keyDown", getall, {filter = tes3.scanCode.g})
+event.register("keyDown", getall, {filter = tes3.scanCode.g})]]
 
 event.register("equipped", amuletEquipped)
 event.register("loaded", loadDataAndCheckMod)
